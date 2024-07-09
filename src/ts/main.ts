18bc9b3de75f33c50eaba51606294a5d3500f677
Benjamin Wright <BenWrightSWE@gmail.com>
2024-07-09 02:46:05 -0400

    finished a majority of the landing page for the mobile size.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -37,12 +37,14 @@ function colorSwitch() {
         root.style.setProperty("--headerBackgroundColor", "#393646");
         root.style.setProperty("--headerNavBackgroundColor", "#4F4557");
         root.style.setProperty("--headerNavButtonColor", "#424242");
-        root.style.setProperty("--textColor", "#F4EEE0");
+        root.style.setProperty("--headerTextColor", "#F4EEE0");
+        root.style.setProperty("--bodyTextColor", "#e6e1d5");
     } else {
         root.style.setProperty("--bodyBackgroundColor", "#EEE9E3");
         root.style.setProperty("--headerBackgroundColor", "#C5A586");
         root.style.setProperty("--headerNavBackgroundColor", "#E7D6C6");
         root.style.setProperty("--headerNavButtonColor", "#D4D6D5");
-        root.style.setProperty("--textColor", "#474747");
+        root.style.setProperty("--headerTextColor", "#474747");
+        root.style.setProperty("--bodyTextColor", "#363535");
     }
 }
